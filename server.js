6ec8b32c5e8e220e316cfda8526698b744d0ab0b
Stephan Seidt <evilhackerdude@gmail.com>
2011-04-07 02:18:45 +0200

    Minor cleanup

--- server.js
+++ server.js
@@ -5,10 +5,6 @@ var spawn = require('child_process').spawn;
 var tty = require('./fixed_tty');
 var url = require('url');
 
-// add session & differnt repls, i.e.: /v8/$session_id
-// put stuff into functions
-// reorganize files
-
 var files = {
 	client: fs.readFileSync('client.html', 'utf8'),
 	dojo: fs.readFileSync('dojo.js', 'utf8'),
@@ -31,25 +27,21 @@ var server = http.createServer(function(req, res){
 	default:
 		res.end('404');
 	}
-	res.end();
-});
-server.listen(1337, '127.0.0.1');
+}).listen(1337, '127.0.0.1');
 
-// kinda works: clj, irb, ipython, ghci
-// doesnt work: v8, node, spidermonkey
+var tty_handle = tty.open('v8');
+var stream = tty_handle[0];
+var repl = tty_handle[1];
 
-var tty_repl = tty.open('v8');
-var stream = tty_repl[0];
-var repl = tty_repl[1];
-stream.setEncoding('utf8');
 repl.on('exit', function(){
 	console.log('repl died');
 });
 
 var socket = io.listen(server, { log: false });
 
-var buffer = '';
+var buffer = ''; // how not to do a buffer.
 
+stream.setEncoding('utf8');
 stream.on('data', function(data){
 	buffer += data;
 	socket.broadcast(data);
@@ -58,6 +50,6 @@ stream.on('data', function(data){
 socket.on('connection', function(client){
 	client.send(buffer);
 	client.on('message', function(message){
-		stream.write(message + "\n");
+		stream.write(message + '\n');
 	});
 });
